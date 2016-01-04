69efe5b11fa5c60200cb7ff6526f17f73401f17e
Kev <kev@teambo.com>
2016-01-04 03:21:15 -0800

    Rebuilding main.go :(

--- main.go
+++ main.go
@@ -1,29 +1,60 @@
 package main
 
 import (
-    // "net/http"
-    "fmt"
+    "net/http"
+    // "fmt"
 	// "log"
-    // "flag"
-	// "golang.org/x/net/http2"
-	// "fmt"
+    "flag"
+	"golang.org/x/net/http2"
+	"fmt"
 	// "errors"
     "github.com/boltdb/bolt"
 )
 
-func list_keys(bucket_name string) {
-    db, err := bolt.Open("/var/lib/teambo/global.db", 0644, nil)
-    if err != nil { return err }
-    defer db.Close()
-	b := tx.Bucket([]byte(bucket_name)).Cursor()
-	fmt.Println(bucket_name)
-	prefix := []byte("")
-	for k, _ := b.Seek(prefix); bytes.HasPrefix(k, prefix); k, _ = b.Next() {
-		fmt.Println(k);
-	}
+type Response map[string]interface{}
+
+var (
+    config map[string]string
+)
+
+var config_path *string = flag.String("conf", "app.conf", "Location of config file")
+var config_version *string = flag.String("v", "12345", "Version")
+
+type StaticHandler struct{}
+func (h StaticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Cache-Control", "max-age=0, must-revalidate")
+    switch r.URL.Path {
+        case "/":                  handle_index(w, r)
+        case "/acct":              handle_acct(w, r)
+        case "/acct/auth":         handle_acct_auth(w, r)
+        case "/acct/verification": handle_acct_verification(w, r)
+        case "/slow":              handle_slow(w, r)
+        case "/app.manifest":      handle_manifest(w, r)
+        case "/init.js":       	   handle_init(w, r)
+        default:                   handle_static(w, r)
+    }
 }
 
 func main() {
-	list_keys("acct")
-	list_keys("verification")
+    flag.Parse()
+    config = parseConfig(*config_path)
+    
+    db_update(func(tx *bolt.Tx) error { 
+		tx.CreateBucketIfNotExists([]byte("acct"))
+		tx.CreateBucketIfNotExists([]byte("verification"))
+        return nil
+    })
+    
+    http.Handle("/", StaticHandler{})
+    
+	if config["ssl.active"] == "true" {
+		srv := &http.Server{
+			Addr:    ":443",
+		}
+		err := http2.ConfigureServer(srv, &http2.Server{})
+		if err != nil { fmt.Println("ERROR - " + err.Error()) }
+	} else {
+		err := http.ListenAndServe(":80", nil)
+		if err != nil { fmt.Println("ERROR - " + err.Error()) }
+	}
 }
