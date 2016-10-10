73d19b0396efee2f3e8e8cceb6f664bf74aaf740
Kev <kev@teambo.com>
2016-10-09 22:59:23 -0700

    Breaking up item test

--- s/js/test/t.team.item.test.js
+++ s/js/test/t.team.item.test.js
@@ -1,5 +1,5 @@
 describe("t.team.item.js", function() {
-    
+
     it("Creates item", function(done) {
         var bucket_id = Teambo.team.current.bucket_ids[0];
         Teambo.team.item.create(bucket_id, {name: 'New Test Item'}).then(function(item){
@@ -10,7 +10,7 @@ describe("t.team.item.js", function() {
             done();
         });
     });
-    
+
     it("Edits item name", function(done) {
         var bucket_id = Teambo.team.current.bucket_ids[0];
         var item_id   = Teambo.team.current.buckets[bucket_id].item_ids[0];
@@ -22,13 +22,28 @@ describe("t.team.item.js", function() {
             done();
         });
     });
-    
+
     it("Deletes item", function(done) {
         var bucket_id = Teambo.team.current.bucket_ids[0];
         Teambo.team.item.create(bucket_id, {name: 'Test Item For Deletion'}).then(function(item){
             Teambo.team.item.remove(bucket_id, item.id).then(function(b){
                 expect(Teambo.team.current.buckets[bucket_id].item_ids.length).toBe(1);
-                // Item uncaches on delete
+                done();
+            }).catch(function(e){
+                fail("Item could not be deleted");
+                done();
+            });
+        }).catch(function(e){
+            fail("Item could not be created");
+            done();
+        });
+    });
+
+    it("Removes item from cache upon deletion", function(done) {
+        var bucket_id = Teambo.team.current.bucket_ids[0];
+        Teambo.team.item.create(bucket_id, {name: 'Test Item For Deletion'}).then(function(item){
+            Teambo.team.item.remove(bucket_id, item.id).then(function(b){
+                expect(Teambo.team.current.buckets[bucket_id].item_ids.length).toBe(1);
                 var hash = Teambo.crypto.sha(Teambo.team.current.id+bucket_id+item.id+Teambo.salt);
                 localforage.getItem(hash).then(function(val){
                     expect(val).toBe(null);
@@ -43,5 +58,5 @@ describe("t.team.item.js", function() {
             done();
         });
     });
-    
+
 });
