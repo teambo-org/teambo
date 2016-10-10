1d735f9e8c92ff3e260a22affb238e5b5afd5f9a
Kev <kev@teambo.com>
2016-10-09 18:19:24 -0700

    Adding bucket tests

--- s/js/test/t.team.bucket.test.js
+++ s/js/test/t.team.bucket.test.js
@@ -1,14 +1,39 @@
-describe("t.acct.js", function() {
-    
-    it("Creates buckets", function(done) {
-        var t = Teambo.acct.current.teams[0];
-        Teambo.acct.current.team.find(t.id).then(function(team){
-            expect(team.opts.name).toBe("Test Team");
+describe("t.team.bucket.js", function() {
+
+    it("Creates bucket", function(done) {
+        Teambo.team.bucket.create('New Test Bucket').then(function(b){
+            expect(b.opts.name).toBe("New Test Bucket");
             done();
         }).catch(function(e){
             fail("Team not found");
             done();
         });
     });
-    
+
+    it("Edits bucket name", function(done) {
+        var bucket_id = Teambo.team.current.bucket_ids[0];
+        Teambo.team.bucket.update(bucket_id, {name: 'Test Bucket'}).then(function(b){
+            expect(b.opts.name).toBe("Test Bucket");
+            done();
+        }).catch(function(e){
+            fail("Team not found");
+            done();
+        });
+    });
+
+    it("Deletes bucket", function(done) {
+        Teambo.team.bucket.create('Bucket to Delete').then(function(b){
+            Teambo.team.bucket.remove(b.id).then(function(b){
+                expect(Teambo.team.current.bucket_ids.length).toBe(1);
+                done();
+            }).catch(function(e){
+                fail("Team not found");
+                done();
+            });
+        }).catch(function(e){
+            fail("Team not found");
+            done();
+        });
+    });
+
 });
