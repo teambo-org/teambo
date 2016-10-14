729242ed6d0bc20b20a84803eb119785f302ce59
Kev <kev@teambo.com>
2016-10-14 04:04:06 -0700

    Splitting complete and incomplete items on bucket page

--- s/js/t.team.bucket.js
+++ s/js/t.team.bucket.js
@@ -52,6 +52,24 @@ Teambo.team.bucket = (function(t){
             }
             return ret;
         };
+        this.item_list_incomplete = function() {
+            var ret = [];
+            for(var k in self.items) {
+                if(self.items[k].opts.status !== "complete") {
+                    ret.push(self.items[k]);
+                }
+            }
+            return ret;
+        };
+        this.item_list_complete = function() {
+            var ret = [];
+            for(var k in self.items) {
+                if(self.items[k].opts.status === "complete") {
+                    ret.push(self.items[k]);
+                }
+            }
+            return ret;
+        };
     };
 
     bucket.create = function(name) {
