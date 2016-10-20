430ec64f0a8557a702f9e346b317e323e60e397f
Kev <kev@teambo.com>
2016-10-20 11:52:14 -0700

    Removing console log

--- templates/dashboard/right.js
+++ templates/dashboard/right.js
@@ -3,7 +3,6 @@
 
     document.querySelectorAll('#right .progress').forEach(function(el) {
       var remaining = document.createElement('div');
-      console.log(Math.abs(100-parseFloat(el.dataset.progress)));
       remaining.style.width = Math.abs(100-parseFloat(el.dataset.progress)) + "%";
       remaining.style.float = 'right';
       remaining.classList.add('color-15');
