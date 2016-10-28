16ddf1a6c4ef7affe628dfd485c1be793b08904e
Kev <kev@teambo.com>
2016-10-28 13:46:48 -0700

    Fixing javascript error in firefox

--- templates/dashboard/right.js
+++ templates/dashboard/right.js
@@ -1,12 +1,14 @@
 function(t){
     "use strict";
-
-    document.querySelectorAll('#right .progress').forEach(function(el) {
+    
+    var els = document.querySelectorAll('#right .progress');
+    for(var i = 0; els[i]; i++) {
+      var el = els[i];
       var remaining = document.createElement('div');
       remaining.style.width = Math.abs(100-parseFloat(el.dataset.progress)) + "%";
       remaining.style.float = 'right';
       remaining.classList.add('color-15');
       el.appendChild(remaining);
-    });
+    }
 
 }
