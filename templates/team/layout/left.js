96672cc2fbf68824361fa823fa9b36ba5c63962b
Kev <kev@teambo.com>
2016-11-02 16:10:02 -0700

    Update styles

--- templates/team/layout/left.js
+++ templates/team/layout/left.js
@@ -3,12 +3,7 @@ function(t){
     
   var els = document.querySelectorAll('#left .progress');
   for(var i = 0; els[i]; i++) {
-    var el = els[i];
-    var remaining = document.createElement('div');
-    remaining.style.width = Math.abs(100-parseFloat(el.dataset.progress)) + "%";
-    remaining.style.float = 'left';
-    remaining.classList.add('color-10');
-    el.appendChild(remaining);
+    els[i].style.width = Math.abs(100-parseFloat(els[i].dataset.progress)) + "%";
   }
 
 }
--- templates/team/layout/right.js
+++ templates/team/layout/right.js
@@ -3,12 +3,7 @@ function(t){
     
   var els = document.querySelectorAll('#right .progress');
   for(var i = 0; els[i]; i++) {
-    var el = els[i];
-    var remaining = document.createElement('div');
-    remaining.style.width = Math.abs(100-parseFloat(el.dataset.progress)) + "%";
-    remaining.style.float = 'right';
-    remaining.classList.add('color-15');
-    el.appendChild(remaining);
+    els[i].style.width = Math.abs(100-parseFloat(els[i].dataset.progress)) + "%";
   }
 
 }
