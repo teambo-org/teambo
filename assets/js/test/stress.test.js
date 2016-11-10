bc5f1daa5a6f6df13f60092afe35fb77893ba04e
Kev <kev@teambo.com>
2016-11-10 05:39:09 -0800

    Updating stress test

--- assets/js/test/stress.test.js
+++ assets/js/test/stress.test.js
@@ -1,13 +1,16 @@
-describe("Stress", function() {
+xdescribe("Stress", function() {
 
   // var factor = 100;
-  var factor = 0;
+  var factor = 50;
 
   it("Performs well with "+factor+" buckets each having "+factor+" items", function(done) {
-    done();
-    return;
     var i = 0;
     var j = 0;
+    var el = document.createElement('div');
+    el.id = 'progress';
+    el.style.padding = '2em';
+    el.style.fontSize = '2em';
+    window.document.body.appendChild(el);
     var create_buckets = function() {
       return new Promise(function(fulfill, reject) {
         Teambo.model.bucket.create({name: 'Test Bucket '+i}).then(function(bucket) {
@@ -29,6 +32,7 @@ describe("Stress", function() {
       return new Promise(function(fulfill, reject) {
         Teambo.model.item.create({name: 'Test Item ' + j, status: 'ready', description: 'This is a test item', bucket_id: bucket.id}).then(function() {
           j++;
+          el.innerHTML = 'Created objects: ' + (j + i*factor);
           if(j < factor) {
             create_items(bucket).then(function(){
               fulfill();
