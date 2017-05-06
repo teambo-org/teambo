65d4173a7031e3250fc356a5f978e99127b41b38
Kev <kev@teambo.com>
2017-05-06 14:59:03 -0700

    Adding autofilter to plan index page and hiding empty buckets after filter

--- assets/js/view/autofilter.js
+++ assets/js/view/autofilter.js
@@ -4,57 +4,59 @@ Teambo.view.autofilter = (function(t){
   var autofilter_cache = {};
 
   var autofilter = function(form, target, sticky) {
-    var tar = document.getElementById(target);
-    var uri = new Uri(window.location.hash.slice(1));
-    var path = uri.path().split('..')[0];
-    var cacheKey = 'filter-'+path
-    var submit_fn = function() {
-      var query = form.serialize();
-      t.team.cache('filter-'+path, query.getQueryParams());
-      window.location.hash = '#' + path + query;
-    };
-    form.addEventListener("submit", submit_fn);
-    var p = [];
-    var data = uri.getQueryParams();
-    var filter = function(data) {
-      var default_options = true;
-      autofilter_cache[cacheKey] = data;
-      var els = form.querySelectorAll('select');
-      [].forEach.call(els, function(el, i) {
-        if(el.name in data) {
-          if(el.value != data[el.name]) {
-            default_options = false;
+    return t.promise(function(fulfill, reject) {
+      var tar = document.getElementById(target);
+      var uri = new Uri(window.location.hash.slice(1));
+      var path = uri.path().split('..')[0];
+      var cacheKey = 'filter-'+path
+      var submit_fn = function() {
+        var query = form.serialize();
+        t.team.cache('filter-'+path, query.getQueryParams());
+        window.location.hash = '#' + path + query;
+      };
+      form.addEventListener("submit", submit_fn);
+      var p = [];
+      var data = uri.getQueryParams();
+      var filter = function(data) {
+        var default_options = true;
+        autofilter_cache[cacheKey] = data;
+        var els = form.querySelectorAll('select');
+        [].forEach.call(els, function(el, i) {
+          if(el.name in data) {
+            if(el.value != data[el.name]) {
+              default_options = false;
+            }
+            el.value = data[el.name];
           }
-          el.value = data[el.name];
-        }
-        tar.classList.add('filter-' + el.name + '-' + el.value);
-        el.addEventListener('change', function() {
-          t.event.once('nav', function() {
-            document[form.name][el.name].focus();
+          tar.classList.add('filter-' + el.name + '-' + el.value);
+          el.addEventListener('change', function() {
+            t.event.once('nav', function() {
+              document[form.name][el.name].focus();
+            });
+            submit_fn();
           });
-          submit_fn();
         });
-      });
-      if(default_options) {
-        form.classList.add('default-options');
-      }
-    };
-    if(sticky && Object.keys(data).length === 0) {
-      if(cacheKey in autofilter_cache) {
-        filter(autofilter_cache[cacheKey]);
+        if(default_options) {
+          form.classList.add('default-options');
+        }
+        fulfill(form);
+      };
+      if(sticky && Object.keys(data).length === 0) {
+        if(cacheKey in autofilter_cache) {
+          filter(autofilter_cache[cacheKey]);
+        } else {
+          t.team.findCached(cacheKey).then(function(val) {
+            if(val) {
+              filter(val);
+            } else {
+              filter(data);
+            }
+          });
+        }
       } else {
-        t.team.findCached(cacheKey).then(function(val) {
-          if(val) {
-            filter(val);
-          } else {
-            filter(data);
-          }
-        });
+        filter(data);
       }
-    } else {
-      filter(data);
-    }
-    return form;
+    });
   };
 
   return autofilter;
--- templates/team/plan/index.js
+++ templates/team/plan/index.js
@@ -3,10 +3,13 @@ function(t){
 
   t.view.progress('#main .progress');
 
+  var filter_form = t.form(document.filter);
+  t.view.autofilter(filter_form, 'plan-index', true);
+
   t.view.on(['plan-removed', 'plan-updated'], function(e) {
     t.refresh({silent: true});
   });
-  
+
   t.view.on(['item-removed', 'item-updated'], function(e) {
     if(e.item.opts.plan_id == t.model.plan.current.id) {
       t.refresh({silent: true});
