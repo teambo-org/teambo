74a3a54e1556fc148dd7b733a3a1bda5ef5e61ee
Kev <kev@teambo.com>
2017-05-07 14:44:27 -0700

    Updating themes and user icons

--- assets/js/t.themes.js
+++ assets/js/t.themes.js
@@ -18,17 +18,6 @@ Teambo.themes = (function(){
       "chat":  {"bg": h2r(h3, 0.5*s, 0.1*v), "hi": h2r(h3, 1*s, 1*v), "color": "255,255,255"}
     };
   };
-  var soft = function(h, s, v) {
-    s = s || 1;
-    v = v || 1;
-    var h2 = h;
-    var h3 = h;
-    return {
-      "main":  {"bg": h2r(h3, 0.9*s, 0.35*v), "hi": h2r(h, 0.9*s, 1*v), "color": "255,255,255"},
-      "right": {"bg": h2r(h2, 0.9*s, 0.5*v), "hi": h2r(h2, 0.9*s, 1*v), "color": "255,255,255"},
-      "chat":  {"bg": h2r(h3, 0.9*s, 0.4*v), "hi": h2r(h3, 0.9*s, 1*v), "color": "255,255,255"}
-    };
-  };
   var deep = function(h, s, v) {
     s = s || 1;
     v = v || 1;
@@ -39,6 +28,13 @@ Teambo.themes = (function(){
       "right": {"bg": h2r(h2, 0.9*s, 0.2*v), "hi": h2r(h2, 0.8*s,  1*v), "color": "255,255,255"},
       "chat":  {"bg": h2r(h3, 0.9*s, 0.2*v), "hi": h2r(h3, 0.75*s, 1*v), "color": "255,255,255"}
     };
+  };
+  var mix = function(method, h1, s1, h2, s2) {
+    return {
+      "main":  {"bg": h2r(h1, s1, 0.15),  "hi": h2r(h2,  s2,  1), "color": "255,255,255"},
+      "right": {"bg": method(h1, s1).right.bg, "hi": method(h2, s2).right.hi, "color": "255,255,255"},
+      "chat":  {"bg": method(h1, s1).chat.bg,  "hi": method(h2, s2).chat.hi, "color": "255,255,255"}
+    }
   };
 
   return {
@@ -55,31 +51,12 @@ Teambo.themes = (function(){
     'Slate Magenta'    : slate(300, 0.9),
     'Slate Crimson'    : slate(330, 1),
 
-    'Deep Red'        : deep(0, 1),
-    'Deep Orange'     : deep(30, 1),
-    'Deep Yellow'     : deep(60, 1),
-    'Deep Chartreuse' : deep(90, 1),
-    'Deep Green'      : deep(120, 1),
-    'Deep Azure'      : deep(155, 1),
-    'Deep Teal'       : deep(180, 1),
-    'Deep Aqua'       : deep(210, 1),
-    'Deep Blue'       : deep(240, 0.7),
-    'Deep Violet'     : deep(270, 0.8),
-    'Deep Magenta'    : deep(300, 1),
-    'Deep Crimson'    : deep(330, 1),
-
-    'Soft Red'        : soft(0,   0.8,  0.8),
-    'Soft Orange'     : soft(30,  0.9,  0.8),
-    'Soft Yellow'     : soft(60,  0.8,  0.8),
-    'Soft Chartreuse' : soft(90,  0.9,  0.8),
-    'Soft Green'      : soft(120, 0.8,  0.8),
-    'Soft Azure'      : soft(155, 1,    0.8),
-    'Soft Teal'       : soft(180, 1,    0.8),
-    'Soft Aqua'       : soft(210, 0.9,  0.8),
-    'Soft Blue'       : soft(240, 0.7, 0.8),
-    'Soft Violet'     : soft(270, 0.8,  0.8),
-    'Soft Magenta'    : soft(300, 0.8,  0.8),
-    'Soft Crimson'    : soft(330, 0.8,  0.8),
+    'Blood Orange' : mix(deep, 0,   1,   30,  1),
+    'Spring Green' : mix(deep, 120, 1,   60,  1),
+    'Ocean Blue'   : mix(deep, 210, 1,   180, 1),
+    'Permafrost'   : mix(deep, 180, 1,   155, 0.9),
+    'Lavender'     : mix(deep, 270, 0.8, 270, 0.5),
+    'Grape Soda'   : mix(deep, 270, 0.8, 300, 1),
 
     Default : slate(180)
   };
