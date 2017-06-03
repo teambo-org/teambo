9faaba286d5100b297aaab8ae3136e393c5808c6
Kev <kev@teambo.com>
2017-06-02 20:52:55 -0700

    Fixing stripe test

--- templates/external/test/stripes.js
+++ templates/external/test/stripes.js
@@ -1,8 +1,15 @@
 function(t){
   "use strict";
 
-  window.fractal.stripes(true);
-  window.fractal.reset();
-  window.fractal.unpause();
+  var run = function() {
+    if(window.fractal) {
+      window.fractal.stripes(true);
+      window.fractal.reset();
+      window.fractal.unpause();
+    } else {
+      setTimeout(run, 100);
+    }
+  };
+  setTimeout(run, 100);
 
 }
